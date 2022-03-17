add90967221557537d38f32e8a59bc55b1e0ff83
Cinnie She <cinnieshe126@gmail.com>
2022-03-17 23:51:13 +0800

    Navigation from home to course fragments

--- app/src/main/java/com/example/year3spring/ui/home/HomeCourseAdapter.kt
+++ app/src/main/java/com/example/year3spring/ui/home/HomeCourseAdapter.kt
@@ -5,6 +5,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.year3spring.R
 import com.example.year3spring.ui.home.data.Course
@@ -15,6 +17,7 @@ class HomeCourseAdapter(private val dataSet: List<Course>) :
     class CourseViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val courseTitle : TextView = view.findViewById(R.id.course_title)
         val courseIcon : ImageView = view.findViewById(R.id.course_icon)
+        val card : CardView = view.findViewById(R.id.home_course_list_item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
@@ -27,6 +30,13 @@ class HomeCourseAdapter(private val dataSet: List<Course>) :
     override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
         holder.courseIcon.setImageResource(dataSet[position].courseIconId)
         holder.courseTitle.setText(dataSet[position].courseTitleId)
+        holder.card.setOnClickListener {
+            when(dataSet[position].courseTitleId){
+                R.string.fina -> it.findNavController().navigate(R.id.action_nav_home_to_nav_fina)
+                R.string.comp_org -> it.findNavController().navigate(R.id.action_nav_home_to_nav_compOrg)
+                // #TODO("4 navigation to go here & have to fix error - not create 2 course fragment from different starting points")
+            }
+        }
     }
 
     override fun getItemCount() = dataSet.size
